d40c78783f0d30dbd3aecd252b5693e3c703753b
Elijah Herald <eherald@magnumpiering.com>
2026-07-07 08:18:47 -0400

    added frontend dist

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
 
 let notes = [
   {
@@ -48,12 +58,26 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
