06a2ac1073132407567baf375d6f68a640950188
mpi-eherald <eherald@magnumpiering.com>
2026-07-08 16:04:16 -0400

    create dev branch

--- part3/index.js
+++ part3/index.js
@@ -13,23 +13,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: '2',
-    content: 'Browser can only execute JavaScript',
-    important: false
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+let notes = []
 
 // FIXME update new backend
 app.get('/', (request, response) => {
