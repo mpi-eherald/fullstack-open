8a2301451ab7b51522fc2d34b63dae238ddfec22
mpi-eherald <eherald@magnumpiering.com>
2026-07-08 15:14:05 -0400

    backend_build

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const Note = require('./models/note')
 const app = express()
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -31,25 +31,25 @@ let notes = [
   }
 ]
 
+// FIXME update new backend
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
 })
 
+// FIXME update new backend
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
+// FIXME update new backend
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(note => note.id !== id)
@@ -66,18 +66,17 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
